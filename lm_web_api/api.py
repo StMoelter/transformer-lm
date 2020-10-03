909b4c9e08715150eff51a29d7a83384c47eb743
Steffen Moelter <steffen@moelter-online.de>
2020-10-04 00:12:17 +0200

    python serves static react build dir

--- lm_web_api/api.py
+++ lm_web_api/api.py
@@ -1,6 +1,6 @@
 import argparse
-from flask import Flask
-from flask import request, jsonify
+from flask import Flask, Response
+from flask import request, jsonify, send_from_directory
 from flask_httpauth import HTTPTokenAuth
 from os import environ
 from os import path
@@ -14,8 +14,14 @@ from lm_web_api.connector import ModelApiConnector
 
 AUTH_TOKEN = environ['AUTH_TOKEN'] if 'AUTH_TOKEN' in environ else '337b14a7-5865-4b24-a2f0-44d98133c860'
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='/gpt-2-ui/build/static')
 auth = HTTPTokenAuth(scheme='Bearer')
+build_path = path.join(path.abspath(path.dirname(__file__)), 'gpt-2-ui', 'build')
+mimetypes = {
+    ".css": "text/css",
+    ".html": "text/html",
+    ".js": "application/javascript",
+}
 
 @auth.verify_token
 def verify_token(token):
@@ -24,7 +30,19 @@ def verify_token(token):
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
 def catch_all(path):
-    return 'You want path: %s' % path
+    if path == '/':
+        path = 'index.html'
+    print("##############################")
+    print(path)
+    print("##############################")
+    print('')
+    return send_from_directory('gpt-2-ui/build', path)
+    # mimetype = mimetypes.get(os.path.splitext(path)[1], "text/html")
+    # try:
+    #    content = open(os.path.join(build_path, path)).read()
+    # except IOError:
+    #    content =
+    # return Response(content, mimetype=mimetype)
 
 @app.route('/gpt2', methods=['POST'])
 @auth.login_required
