75058346e707730bb3f9a24d0a0a1d655821d3b8
Steffen Moelter <steffen@moelter-online.de>
2020-10-04 14:48:02 +0200

    add build dir from react

---
+++ lm_web_api/gpt-2-ui/build/precache-manifest.0f8a6fcc47440ab699db2ea2b015032a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "178312fffff8882268615bf200c73c3f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "40d9b1c6a7786f98f571",
+    "url": "/static/css/2.a544cbf9.chunk.css"
+  },
+  {
+    "revision": "40d9b1c6a7786f98f571",
+    "url": "/static/js/2.20e47651.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.20e47651.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bae398616638e7fda5a5",
+    "url": "/static/js/main.2ad34a98.chunk.js"
+  },
+  {
+    "revision": "27d492839cb5b1ddf41d",
+    "url": "/static/js/runtime-main.8937bb23.js"
+  }
+]);
